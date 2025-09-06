008026285da2510be68070c22e3705430feadd84
Alejandro Malavet <alejandrodavidmalavet@gmail.com>
2025-09-06 17:19:38 -0500

    add network applet

--- .config/nvim/lua/alejandro/plugins/conform.lua
+++ .config/nvim/lua/alejandro/plugins/conform.lua
@@ -14,6 +14,7 @@ return {
                 javascriptreact = { "prettierd" },
 				javascript = { "prettierd" },
 				json = { "prettierd" },
+				jsonc = { "prettierd" },
 				css = { "prettierd" },
 				html = { "prettierd" },
 				yaml = { "prettierd" },
