b1e766d3b67b9c9536b6856ff9605958557175b7
Alejandro Malavet <alejandro.malavet@grafana.com>
2025-12-11 13:36:15 -0500

    cleanups for ai

--- .config/nvim/lua/alejandro/plugins/copilot.lua
+++ .config/nvim/lua/alejandro/plugins/copilot.lua
@@ -1,20 +1,21 @@
 return {
 	"github/copilot.vim",
 	config = function()
-		vim.keymap.set("n", "<leader>ai", function()
-			-- Get current buffer info
-			local bufnr = vim.api.nvim_get_current_buf()
-			local filename = vim.api.nvim_buf_get_name(bufnr)
-
-			-- Construct and execute the cursor CLI command
-			local cmd = string.format(
-				"cursor --reuse-window . %s",
-				filename -- Full path to file
-			)
-
-			vim.fn.system(cmd)
-			print("Opening in Cursor App...")
-		end, { noremap = true, silent = true, desc = "Open in Cursor IDE" })
+		-- Old keymap to open in Cursor IDE
+		-- vim.keymap.set("n", "<leader>ai", function()
+		-- 	-- Get current buffer info
+		-- 	local bufnr = vim.api.nvim_get_current_buf()
+		-- 	local filename = vim.api.nvim_buf_get_name(bufnr)
+		--
+		-- 	-- Construct and execute the cursor CLI command
+		-- 	local cmd = string.format(
+		-- 		"cursor --reuse-window . %s",
+		-- 		filename -- Full path to file
+		-- 	)
+		--
+		-- 	vim.fn.system(cmd)
+		-- 	print("Opening in Cursor App...")
+		-- end, { noremap = true, silent = true, desc = "Open in Cursor IDE" })
 
 		vim.keymap.set("i", "<S-Right>", 'copilot#AcceptWord("\\<Tab>")', {
 			expr = true,
