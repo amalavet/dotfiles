0021b362f8f6db3d7f6daa2bfb6c7dd1f64a52f2
Alejandro Malavet <alejandro.malavet@grafana.com>
2026-01-29 11:55:43 -0600

    fix to exact match on tmux session lookup

--- cmds.zsh
+++ cmds.zsh
@@ -64,7 +64,7 @@ function :to() {
     dir="$1"
     dir_name=$(basename "$dir")
 
-    if tmux has-session -t $dir_name 2>/dev/null; then
+    if tmux has-session -t "=$dir_name" 2>/dev/null; then
         echo "Session $dir_name already exists. Attaching to it."
         tmux attach-session -t $dir_name
     else
