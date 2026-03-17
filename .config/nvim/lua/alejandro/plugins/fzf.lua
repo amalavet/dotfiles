ddb702b89598cb2293246875cb1e0f9fc24dc686
Alejandro Malavet <alejandro.malavet@grafana.com>
2026-03-17 15:31:21 -0600

    cleanup rg

--- .config/nvim/lua/alejandro/plugins/fzf.lua
+++ .config/nvim/lua/alejandro/plugins/fzf.lua
@@ -6,28 +6,32 @@ return {
 		fzf.setup({
 			"telescope",
 		})
+		local ignore_patterns = {
+			".svg",
+			"__pycache__/",
+			".git/",
+			".ttf",
+			".dot",
+			"node_modules/",
+			".DS_Store",
+			".svelte-kit",
+		}
 
 		vim.keymap.set("n", "<leader>ff", function()
 			fzf.files({
-				rg_opts = [[--color=never --files --hidden --follow -g "!.git" --no-ignore]],
-				fd_opts = "--color=never --type f --hidden --follow --exclude .git --no-ignore",
-				-- This uses lua pattern matching: https://www.lua.org/pil/20.2.html
-				file_ignore_patterns = {
-					".svg",
-					"__pycache__/",
-					".git/",
-					".ttf",
-					".dot",
-					"node_modules/",
-					".DS_Store",
-					".svelte-kit",
-				},
+				file_ignore_patterns = ignore_patterns,
 			})
 		end, { desc = "FZF: Find Files" })
 		vim.keymap.set("n", "<leader>fg", fzf.git_files, { desc = "FZF: Git Files" })
 		vim.keymap.set("n", "<leader>fb", fzf.buffers, { desc = "FZF: Buffers" })
 		vim.keymap.set("n", "<leader>fh", fzf.help_tags, { desc = "FZF: Get Help" })
-		vim.keymap.set("n", "<leader>fl", fzf.live_grep, { desc = "FZF: Live Grep" })
+		vim.keymap.set("n", "<leader>fl", function()
+			fzf.live_grep({
+				rg_opts = [[--hidden]],
+				file_ignore_patterns = ignore_patterns,
+				silent = true,
+			})
+		end, { desc = "FZF: Live Grep" })
 		vim.keymap.set("n", "<leader>fk", fzf.keymaps, { desc = "FZF: Keymaps" })
 		vim.keymap.set("n", "<leader>fw", fzf.grep_cword, { desc = "FZF: Grep Current Word" })
 		vim.keymap.set("n", "<leader>fW", fzf.grep_cWORD, { desc = "FZF: Grep Current WORD" })
