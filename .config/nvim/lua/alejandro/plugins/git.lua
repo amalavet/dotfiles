00ff2eb3a1209d34f3ef76320ff6e80d3b625f79
Alejandro Malavet <alejandro.malavet@grafana.com>
2026-07-28 10:22:59 -0500

    fix go github cmd

--- .config/nvim/lua/alejandro/plugins/git.lua
+++ .config/nvim/lua/alejandro/plugins/git.lua
@@ -5,7 +5,8 @@ return {
 		src = "https://github.com/tpope/vim-fugitive",
 		config = function()
 			vim.keymap.set("n", "<leader>gs", "<cmd>Gvdiffsplit<CR>", { desc = "Git: See diff in split view" })
-			vim.keymap.set("n", "<leader>go", "<cmd>GBrowse<CR>", { desc = "Git: Open in browser" })
+			vim.keymap.set("n", "<leader>go", "<cmd>.GBrowse<CR>", { desc = "Git: Open in browser" })
+			vim.keymap.set("v", "<leader>go", ":GBrowse<CR>", { desc = "Git: Open in browser" })
 
 			local signs = {
 				add = { text = "" },
